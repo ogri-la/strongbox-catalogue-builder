32f71aa0aa961e0b15360455c8ff4b8c781bdee6
Torkus <48141663+ogri-la@users.noreply.github.com>
2022-11-19 13:16:11 +1030

    constants, date of last release updated

--- src/scb/constants.clj
+++ src/scb/constants.clj
@@ -2,6 +2,6 @@
 
 ;; https://wow.gamepedia.com/Wowpedia
 (def release-of-previous-expansion
-  "'Battle for Azeroth (BfA)', released August 14th 2018. Used to shorten the 'full' catalogue.
+  "'Shadowlands', released November 23rd 2020. Used to shorten the 'full' catalogue.
   https://en.wikipedia.org/wiki/World_of_Warcraft#Expansions"
-  "2018-08-14T00:00:00Z")
+  "2020-11-23T00:00:00Z")
