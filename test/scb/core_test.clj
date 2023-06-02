4e11714d9eb8d147efc85a1eeccfad199b5be4b6
Torkus <48141663+ogri-la@users.noreply.github.com>
2023-06-02 23:08:42 +0930

    linted, fixed a bunch of broken references

--- test/scb/core_test.clj
+++ test/scb/core_test.clj
@@ -2,50 +2,49 @@
   (:require
    ;;[taoensso.timbre :as timbre :refer [debug info warn error spy]]
    [clojure.test :refer [deftest is testing use-fixtures]]
-   [clj-http.fake :refer [with-global-fake-routes-in-isolation]]
+   ;;[clj-http.fake :refer [with-global-fake-routes-in-isolation]]
    [scb.helper :as helper]
    [scb
     [user :as user]
-    [core :as core]
-    ]))
+    [core :as core]]))
 
 (use-fixtures :each helper/no-http)
 (use-fixtures :each helper/temp-state-dir)
 
 ;; 2022-09-11: disabled, adding the landing page now finds all category landing pages and adds them too, blowing out the fake routes
 #_(deftest download-queue
-  (testing "general test of adding urls to be downloaded, parsed, stored"
-    (let [url "https://www.wowinterface.com/addons.php"
-          fixture (helper/fixture-path "wowinterface--landing.html")
-          fake-routes {url {:get (fn [req] {:status 200 :body (slurp fixture)})}}]
-      (with-global-fake-routes-in-isolation fake-routes
-        (helper/with-running-app
-          (core/cleanup) ;; stops any workers
-          (core/run-worker core/download-worker)
-          (core/put-item (core/get-state :download-queue) url)
-          
-          (Thread/sleep 250) ;; 100 is typically enough, but is slower on a first run
+    (testing "general test of adding urls to be downloaded, parsed, stored"
+      (let [url "https://www.wowinterface.com/addons.php"
+            fixture (helper/fixture-path "wowinterface--landing.html")
+            fake-routes {url {:get (fn [req] {:status 200 :body (slurp fixture)})}}]
+        (with-global-fake-routes-in-isolation fake-routes
+          (helper/with-running-app
+            (core/cleanup) ;; stops any workers
+            (core/run-worker core/download-worker)
+            (core/put-item (core/get-state :download-queue) url)
 
-          (is (core/empty-queue? :download-queue))
-          (is (not (core/empty-queue? :downloaded-content-queue)))
+            (Thread/sleep 250) ;; 100 is typically enough, but is slower on a first run
+
+            (is (core/empty-queue? :download-queue))
+            (is (not (core/empty-queue? :downloaded-content-queue)))
 
           ;; don't run a parser-worker, it will parse the content and add new work to download and parse.
           ;; the fake routes will fail, etc.
           ;; instead, parse content manually.
 
-          (let [[item _] (core/take-item (core/get-state :downloaded-content-queue))
-                parsed-content (core/parse-content item)
-                to-download (:download parsed-content)
+            (let [[item _] (core/take-item (core/get-state :downloaded-content-queue))
+                  parsed-content (core/parse-content item)
+                  to-download (:download parsed-content)
 
-                expected 47 ;; category pages to download
-                expected-first-url
-                {:label "The Burning Crusade Classic", :url "https://www.wowinterface.com/downloads/index.php?cid=161&sb=dec_date&so=desc&pt=f&page=1"}
-                expected-last-url
-                {:label "Discontinued and Outdated Mods", :url "https://www.wowinterface.com/downloads/index.php?cid=44&sb=dec_date&so=desc&pt=f&page=1"}]
+                  expected 47 ;; category pages to download
+                  expected-first-url
+                  {:label "The Burning Crusade Classic", :url "https://www.wowinterface.com/downloads/index.php?cid=161&sb=dec_date&so=desc&pt=f&page=1"}
+                  expected-last-url
+                  {:label "Discontinued and Outdated Mods", :url "https://www.wowinterface.com/downloads/index.php?cid=44&sb=dec_date&so=desc&pt=f&page=1"}]
 
-            (is (= expected (count to-download)))
-            (is (= expected-first-url (first to-download)))
-            (is (= expected-last-url (last to-download)))))))))
+              (is (= expected (count to-download)))
+              (is (= expected-first-url (first to-download)))
+              (is (= expected-last-url (last to-download)))))))))
 
 (deftest download-queue--bad-item
   (testing "receiving a bad item from the download queue doesn't cause a crash"
--- test/scb/tukui_test.clj
+++ test/scb/tukui_test.clj
@@ -10,8 +10,7 @@
 
 (deftest tukui-date-to-rfc3339
   (let [cases [["2022-09-11" "2022-09-11T00:00:00Z"]
-               ["2022-09-11 02:05:24" "2022-09-11T02:05:24Z"]]
-        ]
+               ["2022-09-11 02:05:24" "2022-09-11T02:05:24Z"]]]
     (doseq [[given expected] cases]
       (is (= expected (tukui/tukui-date-to-rfc3339 given))))))
 
@@ -26,10 +25,9 @@
                   :tag-list #{:compilations :ui},
                   :updated-date "2019-07-25T17:00:42Z",
                   :url "https://www.tukui.org/addons.php?id=42"}
-        fixture (-> "tukui--retail-addon.json" fixture-path utils/json-slurp first)
-        ]
+        fixture (-> "tukui--retail-addon.json" fixture-path utils/json-slurp first)]
     (is (= expected (tukui/process-tukui-item fixture :retail)))))
-  
+
 (deftest process-tukui-item--classic
   (let [expected {:description "clean and modern UI with Tank/DPS and Heal Layout. transparent and Classcolor Themes are available.",
                   :download-count 5325,
@@ -41,7 +39,6 @@
                   :tag-list #{:elvui :plugins},
                   :updated-date "2020-05-05T20:17:14Z",
                   :url "https://www.tukui.org/classic-addons.php?id=36"}
-        fixture (-> "tukui--classic-addon.json" fixture-path utils/json-slurp first)
-        ]
+        fixture (-> "tukui--classic-addon.json" fixture-path utils/json-slurp first)]
     (is (= expected (tukui/process-tukui-item fixture :classic)))))
 
